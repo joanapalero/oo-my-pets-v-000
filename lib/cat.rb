68b4a20331f8474868724ef1eb863d6ad74cb5d8
Joana Palero <joana.palero24@gmail.com>
2018-07-08 15:34:48 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,8 +3,8 @@ class Cat
   attr_accessor :mood
   attr_accessor :name 
   
-  def initialize(name="mr. whiskers")
+  def initialize(name, mood="nervous")
      @name = name
-     @mood = "nervous"
+     @mood = mood
    end
 end
