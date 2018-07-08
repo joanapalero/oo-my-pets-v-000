6267ee976bdc8301427194516eddb36d9c034b22
Joana Palero <joana.palero24@gmail.com>
2018-07-08 15:43:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,77 +1,69 @@
 require 'pry'
-
 class Owner
-  attr_accessor :name, :pets, :mood
+  @@all = []
+  attr_accessor :name, :pets
   attr_reader :species
 
-  @@all = []
-
   def initialize(name)
-    @name = name
-    @species = "human"
-    @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
+    @species = "human"
+    @pets = {
+      fishes: [],
+      dogs: [],
+      cats: []
+    }
   end
 
   def self.all
     @@all
   end
 
-  def self.count
-    self.all.count
+  def self.reset_all
+    @@all.clear
   end
 
-  def self.reset_all
-    self.all.clear
+  def self.count
+    self.all.length
   end
 
   def say_species
-    "I am a #{@species}."
+    "I am a #{species}."
   end
 
   def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
+    pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
+    pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    # binding.pry
-    pet = @pets[:dogs]
-    pet.select{|dog| dog.mood = "happy"}
-    pet
+    pets[:dogs].each {|dog| dog.mood = "happy"}
   end
 
   def play_with_cats
-    pet = @pets[:cats]
-    pet.select{|cat| cat.mood = "happy"}
-    pet
+    pets[:cats].each {|cat| cat.mood = "happy"}
   end
 
   def feed_fish
-    pet = @pets[:fishes]
-    pet.select{|fish| fish.mood = "happy"}
-    pet
-  end
-
-  def list_pets
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
   def sell_pets
-    # binding.pry
-    @pets.each do |animal, array|
-      array.each{|pet| pet.mood = "nervous"}
-      array.clear
+  pets.each do |type, pets|
+    pets.each do |pet|
+      pet.mood = "nervous"
     end
-
+  end
+  pets.clear
   end
 
-
+  def list_pets
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+  end
 end
