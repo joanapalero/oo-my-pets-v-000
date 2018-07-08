a5db109640bbd6aa64eff6188eae1d63c28e62d3
Joana Palero <joana.palero24@gmail.com>
2018-07-08 15:43:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,69 +1,77 @@
 require 'pry'
+
 class Owner
-  @@all = []
-  attr_accessor :name, :pets
+  attr_accessor :name, :pets, :mood
   attr_reader :species
 
+  @@all = []
+
   def initialize(name)
-    @@all << self
+    @name = name
     @species = "human"
-    @pets = {
-      fishes: [],
-      dogs: [],
-      cats: []
-    }
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
   end
 
   def self.all
     @@all
   end
 
-  def self.reset_all
-    @@all.clear
+  def self.count
+    self.all.count
   end
 
-  def self.count
-    self.all.length
+  def self.reset_all
+    self.all.clear
   end
 
   def say_species
-    "I am a #{species}."
+    "I am a #{@species}."
   end
 
   def buy_fish(name)
-    pets[:fishes] << Fish.new(name)
+    @pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    pets[:cats] << Cat.new(name)
+    @pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    pets[:dogs] << Dog.new(name)
+    @pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    pets[:dogs].each {|dog| dog.mood = "happy"}
+    # binding.pry
+    pet = @pets[:dogs]
+    pet.select{|dog| dog.mood = "happy"}
+    pet
   end
 
   def play_with_cats
-    pets[:cats].each {|cat| cat.mood = "happy"}
+    pet = @pets[:cats]
+    pet.select{|cat| cat.mood = "happy"}
+    pet
   end
 
   def feed_fish
-    pets[:fishes].each {|fish| fish.mood = "happy"}
-  end
-
-  def sell_pets
-  pets.each do |type, pets|
-    pets.each do |pet|
-      pet.mood = "nervous"
-    end
-  end
-  pets.clear
+    pet = @pets[:fishes]
+    pet.select{|fish| fish.mood = "happy"}
+    pet
   end
 
   def list_pets
-    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
+
+  def sell_pets
+    # binding.pry
+    @pets.each do |animal, array|
+      array.each{|pet| pet.mood = "nervous"}
+      array.clear
+    end
+
+  end
+
+
 end
